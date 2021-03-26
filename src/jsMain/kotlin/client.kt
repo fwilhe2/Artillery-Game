439e9290c21ff3e6d271802f135770dc8ed69c66
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-03-26 12:08:08 +0100

    Write text to page

--- src/jsMain/kotlin/client.kt
+++ src/jsMain/kotlin/client.kt
@@ -1,12 +1,15 @@
+import kotlinx.browser.*
+import kotlinx.dom.appendText
+
 fun main() {
     val g = GameWorld()
 
     do {
         performPlayerMove(g)
         println(g)
+        document.body!!.appendText(g.toString())
     }while (g.gameContinues())
 
-
     println(g.determineWinner())
-
+    document.body!!.appendText(g.determineWinner())
 }
